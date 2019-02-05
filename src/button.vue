5074833952e8493812be04e5d67d35354e921c1a
rougua <guakun@aliyun.com>
2019-02-05 13:38:42 +0800

    整理代码

--- src/button.vue
+++ src/button.vue
@@ -68,22 +68,16 @@
   $primary-button-active-background-color: #2baee9;
   $primary-button-active-border-color: #2baee9;
 
-  @keyframes spin {
-    0% { transform: rotate(0deg) }
-    100% { transform: rotate(360deg) }
-  }
+  @keyframes spin { 0% { transform: rotate(0deg) } 100% { transform: rotate(360deg) } }
 
   .g-button {
-    box-sizing: border-box;
-    transition: .3s cubic-bezier(.645, .045, .355, 1);
+    box-sizing: border-box; transition: .3s cubic-bezier(.645, .045, .355, 1);
     font-size: var(--font-size); cursor: pointer; padding: 3px 12px;
     line-height: 1.5; font: inherit; border-radius: $button-border-radius;
     color: $button-color; border: 1px solid $button-border-color; background: $button-background-color;
     text-align: center; vertical-align: middle;
     display: inline-flex; justify-content: center; align-items: center;
-    &, &:active, &:focus {
-      outline: none;
-    }
+    &, &:active, &:focus { outline: none; }
     .loading { animation: spin .3s infinite linear; }
     > .icon { order: 1; margin-right: .1em; }
     > .content { order: 2 }
@@ -91,58 +85,33 @@
       > .icon { order: 2; margin-right: 0; margin-left: .1em; }
       > .content { order: 1 }
     }
-    &.circle {
-      width: 28px;
-      height: 28px;
-      padding: 0;
-      border-radius: 50%;
-    }
+    &.circle { width: 28px; height: 28px; padding: 0; border-radius: 50%; }
 
     &.lg {
-      &.circle {
-        width: 32px;
-        height: 32px;
-        padding: 0;
-        font-size: $font-size-ll;
-      }
-      padding: 4px 15px 5px 15px;
-      font-size: $font-size-l;
-      border-radius: $button-border-radius-l;
+      &.circle { width: 32px; height: 32px; padding: 0; font-size: $font-size-ll; }
+      padding: 4px 15px 5px 15px; font-size: $font-size-l; border-radius: $button-border-radius-l;
     }
     &.sm {
-      &.circle {
-        width: 22px;
-        height: 22px;
-        padding: 0;
-      }
-      padding: 1px 7px;
-      border-radius: $button-border-radius-s;
+      &.circle { width: 22px; height: 22px; padding: 0; }
+      padding: 1px 7px; border-radius: $button-border-radius-s;
     }
 
     &.primary {
-      color: $primary-button-color;
-      border-color: $primary-button-border-color;
-      background: $primary-button-background-color;
+      color: $primary-button-color; border-color: $primary-button-border-color; background: $primary-button-background-color;
       svg { fill: white; }
       &:hover {
         svg { fill: $primary-button-hover-color; }
-        color: $primary-button-hover-color;
-        border-color: $primary-button-hover-border-color;
-        background: $primary-button-hover-background-color;
+        color: $primary-button-hover-color; border-color: $primary-button-hover-border-color; background: $primary-button-hover-background-color;
       }
       &:active {
         svg { fill: $primary-button-active-color; }
-        color: $primary-button-active-color;
-        border-color: $primary-button-active-border-color;
-        background: $primary-button-active-background-color;
+        color: $primary-button-active-color; border-color: $primary-button-active-border-color; background: $primary-button-active-background-color;
       }
     }
 
     &.ghost {
       svg { fill: $button-color; }
-      color: $button-color;
-      border-color: $button-border-color;
-      background: white;
+      color: $button-color; border-color: $button-border-color; background: white;
     }
 
     &:hover {
@@ -152,15 +121,10 @@
 
     &:active {
       svg { fill: $button-active-color; }
-      color: $button-active-color;
-      border: 1px solid $button-active-border-color;
-      background: $button-active-background-color;
+      color: $button-active-color; border: 1px solid $button-active-border-color; background: $button-active-background-color;
     }
     &.disabled, &.primary.disabled {
-      color: #ccc;
-      background-color: #f3f5f7;
-      border-color: #d9d9d9;
-      cursor: not-allowed;
+      color: #ccc; background-color: #f3f5f7; border-color: #d9d9d9; cursor: not-allowed;
     }
   }
 </style>
